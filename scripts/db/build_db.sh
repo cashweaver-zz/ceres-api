83e815f60d24f48be18a0393ae348006db7ba7a0
vagrant <vagrant@vagrant.vm>
2015-12-21 15:26:37 +0000

    Reorganize scripts

---
+++ scripts/db/build_db.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+./download_datasets.sh
---
+++ scripts/db/download_datasets.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+# Download datasets from the NCDC
+
+SAVE_PATH="/home/vagrant/app/data/ncdc/raw"
+mkdir -p "$SAVE_PATH"
+cd "$SAVE_PATH"
+if [[ "$(pwd)" != "$SAVE_PATH" ]]
+then
+  echo "Problem changing directory to $SAVE_PATH"
+  exit 1;
+fi
+echo "Downloading datasets from the NCDC to $SAVE_PATH"
+
+# Documentation
+# ------------------------------------------------
+
+# README: Includes explanations on units, file naming, file structure, and more.
+wget http://www1.ncdc.noaa.gov/pub/data/normals/1981-2010/readme.txt
+
+# Methodology
+wget http://www1.ncdc.noaa.gov/pub/data/normals/1981-2010/documentation/temperature-methodology.pdf
+
+
+# Data
+# ------------------------------------------------
+
+# Stations
+wget http://www1.ncdc.noaa.gov/pub/data/normals/1981-2010/station-inventories/allstations.txt
+wget http://www1.ncdc.noaa.gov/pub/data/normals/1981-2010/station-inventories/zipcodes-normals-stations.txt
+
+# Temperature data
+wget http://www1.ncdc.noaa.gov/pub/data/normals/1981-2010/products/temperature/dly-tmax-normal.txt
+wget http://www1.ncdc.noaa.gov/pub/data/normals/1981-2010/products/temperature/dly-tmax-stddev.txt
+wget http://www1.ncdc.noaa.gov/pub/data/normals/1981-2010/products/temperature/dly-tmin-normal.txt
+wget http://www1.ncdc.noaa.gov/pub/data/normals/1981-2010/products/temperature/dly-tmin-stddev.txt
